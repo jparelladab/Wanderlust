2222ea019f00efefe5a382761e4a0e0519c9ca96
joan parellada <joan.parellada789@gmail.com>
2020-01-21 21:10:09 +0100

    added get venues and get forecast functions

--- Wanderlust.js
+++ Wanderlust.js
@@ -17,12 +17,37 @@ const $weatherDiv = $("#weather1");
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Add AJAX functions here:
-const getVenues = () => {
-
+const getVenues = async () => {
+  const city = $input.val();
+  const urlToFetch = url + city + '&limit=10&client_id=' + clientId +'&client_secret=' + clientSecret + '&v=20200120';
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      const venues =         jsonResponse.response.groups[0].items.map(item => item.venue);
+      console.log(venues);
+      return venues;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const getForecast = () => {
 
+const getForecast = async () => {
+  const urlToFetch = `${weatherUrl}?q=${$input.val()}&APPID=${openWeatherKey}`;
+  const response = await fetch(urlToFetch);
+  if (response.ok) {
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+    return jsonResponse;
+  }
+  try {
+
+  } catch(error) {
+    console.log(error);
+  }
 }
 
 
